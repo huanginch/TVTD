30ac39b533d5621481fe48af21a6b71c82bee5f0
huanginch <huanginch329@gmail.com>
2024-03-27 17:10:06 +0800

    @refactor: Make wall ScriptableObject
    -change wall from Monobehavior to ScriptableObject
    -add wall factory assets
    -modify wall

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    // Start is called before the first frame update
+	// Start is called before the first frame update
+	WallFactory originFactory;
+
+	public WallFactory OriginFactory
+	{
+		get => originFactory;
+		set
+		{
+			Debug.Assert(originFactory == null, "Redefined origin factory!");
+			originFactory = value;
+		}
+	}
+
+	public void SpawnOn(GameTile tile)
+	{
+		Vector3 newPos = new Vector3(tile.transform.localPosition.x, 0.5f, tile.transform.localPosition.z);
+		transform.localPosition = newPos;
+	}
 }
--- Assets/Scripts/WallFactory.cs
+++ Assets/Scripts/WallFactory.cs
@@ -2,75 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallFactory : MonoBehaviour
+[CreateAssetMenu]
+public class WallFactory : GameObjectFactory
 {
-    // Start is called before the first frame update
-    [SerializeField]
-    Wall wallPrefab = new Wall();
+	// Start is called before the first frame update
+	[SerializeField]
+	Wall prefab = default;
 
-	public void createWall(Vector2Int size)
+	public Wall Get()
 	{
-		createTopWall(size);
-		createDownWall(size);
-		createLeftWall(size);
-		createRightWall(size);
+		Wall instance = CreateGameObjectInstance(prefab);
+		instance.OriginFactory = this;
+		return instance;
 	}
 
-	private void createTopWall(Vector2Int size) 
+	public void Reclaim(Wall wall)
 	{
-		Wall[] walls = new Wall[size.x * 1];
-
-		Vector2 offset = new Vector2((size.x / 2) - 0.5f, size.y / 2 + 0.5f); 
-
-		for (int i = 0; i < walls.Length; i++)
-        {
-			Wall wall = walls[i] = Instantiate(wallPrefab);
-			wall.transform.SetParent(transform, false);
-			wall.transform.localPosition = new Vector3(
-				i - offset.x, 0.5f, offset.y
-			);
-		}
-	}
-	private void createLeftWall(Vector2Int size) {
-		Wall[] walls = new Wall[size.y * 1 - 2];
-
-		Vector2 offset = new Vector2(size.x / 2 - 0.5f, size.y / 2 - 1 - 0.5f); //todo: the 12 should the the size of game board
-
-		for (int i = 0; i < walls.Length; i++)
-		{
-			Wall wall = walls[i] = Instantiate(wallPrefab);
-			wall.transform.SetParent(transform, false);
-			wall.transform.localPosition = new Vector3(
-				-offset.x, 0.5f, i - offset.y
-			);
-		}
-	}
-	private void createRightWall(Vector2Int size) {
-		Wall[] walls = new Wall[size.y * 1 - 2];
-
-		Vector2 offset = new Vector2(size.x / 2 - 0.5f, size.y / 2 - 1 - 0.5f); //todo: the 12 should the the size of game board
-
-		for (int i = 0; i < walls.Length; i++)
-		{
-			Wall wall = walls[i] = Instantiate(wallPrefab);
-			wall.transform.SetParent(transform, false);
-			wall.transform.localPosition = new Vector3(
-				offset.x, 0.5f, i - offset.y
-			);
-		}
-	}
-	private void createDownWall(Vector2Int size) {
-		Wall[] walls = new Wall[size.x * 1];
-
-		Vector2 offset = new Vector2((size.x / 2) - 0.5f, size.y / 2 - 1 + 0.5f); 
-
-		for (int i = 0; i < walls.Length; i++)
-		{
-			Wall wall = walls[i] = Instantiate(wallPrefab);
-			wall.transform.SetParent(transform, false);
-			wall.transform.localPosition = new Vector3(
-				i - offset.x, 0.5f, -offset.y
-			);
-		}
+		Debug.Assert(wall.OriginFactory == this, "Wrong factory reclaimed!");
+		Destroy(wall.gameObject);
 	}
 }
